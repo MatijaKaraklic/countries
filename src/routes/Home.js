b05ac16bb4d484e958ced0f611552a61de6ce86a
Matija Karaklic <matija.karaklic25@gmail.com>
2023-07-05 12:39:31 +0200

    search

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,27 +7,53 @@ import './Home.css';
 function Home() {
 
   const [data, setData] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [search, setSearch] = useState('');
 
   // name, flags, capital, population, area
   // ToDo: add img download to await;
   // ?fields=name,flags,capital,population,area
 
   useEffect(() => {
+    console.log("PaPa");
     fetch('https://restcountries.com/v3.1/all?fields=name,flags,capital,population,area,cca3').then(response => response.json()).then(json => {setData(json)});
-  }, [])
+  }, []);
 
+  useEffect(() => {
+    if(data === null) {
+      setLoaded(false);
+    }
+    else setLoaded(true);
+  }, [data]);
+
+  const changeSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const renderCards = (data, search) => {
+    
+    const cards = data.map(country => {
+      const card = (<Card data={{ country: country }} />);
+
+      if(search === '') return card;
+
+      const name = country.name.common.replace(/\s+/g, '').toLowerCase();
+      const searchQuery = search.replace(/\s+/g, '').toLowerCase();
+
+      if(name.includes(searchQuery)) return card;
+    });
+
+    return cards;
+  }
+  
 
   return (
     <div className='Home-body'>
-      {(data === null) ? (null) : (<input type='text' id='search-country' placeholder='Search country:' className='Home-input'/>)}
+      {(data === null) ? (null) : (<input type='text' id='search-country' placeholder='Search country:' className='Home-input' onChange={changeSearch}/>)}
       <div className='container Home-container'>
         {(data === null) ? (
           (<p className='loading'>Loading...</p>)
-        ) : (
-          data.map(country => {
-            return (<Card data={{ country: country }} />);
-          })
-        )}
+        ) : (renderCards(data, search))}
       </div>
     </div>
   );
